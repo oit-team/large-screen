f329045b4ddb73108672212826465e47fde0b3d8
李晓飞 <xiaofeiyu187@163.com>
2022-11-11 14:15:30 +0800

    fix: 解决路由缓存问题

--- packages/legacy/src/App.vue
+++ packages/legacy/src/App.vue
@@ -1,10 +1,29 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data: () => ({
+    include: [],
+  }),
+  watch: {
+    $route() {
+      const name = this.$route.matched.at(-1).components.default.name
+      const keepAlive = this.$route.meta.keepAlive
+      if (keepAlive && name) {
+        this.include = [...new Set([...this.include, name])]
+      }
+    },
+  },
+})
+</script>
+
 <template>
   <div id="legacy">
-    <!-- <Transition name="el-fade-in" mode="out-in"> -->
-    <KeepAlive>
-      <RouterView />
-    </KeepAlive>
-    <!-- </Transition> -->
+    <Transition name="el-fade-in" mode="out-in">
+      <KeepAlive :include="include">
+        <RouterView />
+      </KeepAlive>
+    </Transition>
   </div>
 </template>
 

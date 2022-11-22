becb10e4188f944160afd5ec1f74c44bd1ec57f7
邱旭 <uxuip@outlook.com>
2022-11-22 16:06:52 +0800

    feat: 页面缓存

--- packages/main/src/layout/Main.vue
+++ packages/main/src/layout/Main.vue
@@ -1,19 +1,27 @@
 <script setup>
+import { useRoute } from 'vue-router/composables'
 import Footer from './Footer.vue'
 
 defineProps({
   isReload: Boolean,
 })
+
+const route = useRoute()
 </script>
 
 <template>
   <ElContainer class="overflow-auto" direction="vertical">
     <ElMain class="bg-gray-100 p-3">
       <div class="bg-white rounded-lg h-full">
-        <Transition name="el-fade-in" mode="out-in">
-          <KeepAlive>
-            <RouterView v-if="isReload" class="h-full overflow-auto" />
-          </KeepAlive>
+        <KeepAlive>
+          <Transition v-if="route.meta.keepAlive" name="el-fade-in" mode="out-in">
+            <KeepAlive v-if="isReload">
+              <RouterView class="h-full overflow-auto" />
+            </KeepAlive>
+          </Transition>
+        </KeepAlive>
+        <Transition v-if="!route.meta.keepAlive" name="el-fade-in" mode="out-in">
+          <RouterView v-if="isReload" class="h-full overflow-auto" />
         </Transition>
       </div>
     </ElMain>

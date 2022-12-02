e8248453c85616dbafb562b79bffa2ee21a25b15
刘运双 <1048821976@qq.com>
2022-12-02 14:27:32 +0800

    fix: 修复切换品牌，老项目列表空白

--- packages/main/src/layout/index.vue
+++ packages/main/src/layout/index.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { useRoute } from 'vue-router/composables'
 import Aside from './Aside.vue'
 import Header from './Header.vue'
 import Main from './Main.vue'
 
+const route = useRoute()
 const isReload = ref(true)
 function viewReload() {
+  // 如果是老项目 切换品牌整个页面刷新
+  if (route.name === 'Legacy') {
+    window.location.reload()
+    return
+  }
   isReload.value = false
   setTimeout(() => isReload.value = true)
 }

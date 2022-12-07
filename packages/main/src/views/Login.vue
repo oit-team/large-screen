130c6b0c74a486b722b7d21f5fdf28f8dbedefee
刘运双 <1048821976@qq.com>
2022-12-07 11:17:05 +0800

    fix: 优化登录页、新增登录背景和logo

--- packages/main/src/views/Login.vue
+++ packages/main/src/views/Login.vue
@@ -50,19 +50,22 @@ export default defineComponent({
 <template>
   <div class="login">
     <div class="login-content">
-      <div class="login-logo">
-        <img src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" height="125" width="125">
+      <div class="login-logo flex flex-col justify-center">
+        <img src="/images/login-logo.png" style="width:200px">
+        <h2 class="text-center text-2xl text-gray-500 leading-16">
+          享客易管家
+        </h2>
       </div>
-      <ElForm ref="form" :model="form" :rules="rules" label-width="100px">
+      <ElForm ref="form" :model="form" :rules="rules" label-width="60px">
         <ElFormItem prop="userName">
-          <ElInput v-model="form.userName" type="username" clearable prefix-icon="el-icon-s-custom" />
+          <ElInput v-model="form.userName" type="username" placeholder="请输入用户名" clearable prefix-icon="el-icon-s-custom" />
         </ElFormItem>
         <ElFormItem prop="passWord">
-          <ElInput v-model="form.passWord" type="password" clearable prefix-icon="el-icon-lock" @keyup.enter.native="login" />
+          <ElInput v-model="form.passWord" type="password" placeholder="请输入密码" clearable prefix-icon="el-icon-lock" @keyup.enter.native="login" />
         </ElFormItem>
         <ElFormItem>
           <ElButton type="primary" class="w-full" @click="login">
-            登录
+            登 录
           </ElButton>
         </ElFormItem>
       </ElForm>
@@ -89,6 +92,14 @@ export default defineComponent({
     background-color: #fff;
     box-shadow: 2px 2px 2px rgba(247, 242, 242, 0.501);
     transform: translateX(220px);
+  }
+}
+::v-deep{
+  input::-webkit-input-placeholder {
+    font-size: 12px;
+  }
+  .el-button--primary{
+    background-color: rgb(111 167 255);
   }
 }
 </style>

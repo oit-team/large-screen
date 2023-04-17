f941e74047646e7f054abc99048469873e803052
李晓飞 <xiaofeiyu187@163.com>
2023-04-17 13:34:41 +0800

    fix: footer时间修改

--- packages/main/src/layout/Footer.vue
+++ packages/main/src/layout/Footer.vue
@@ -1,5 +1,9 @@
+<script setup>
+const nowTear = new Date().getFullYear()
+</script>
+
 <template>
   <ElFooter class="flex items-center justify-center text-xs text-dark-100" height="50px">
-    Copyright @2016-2022 上海靖璇网络科技有限公司 版权所有
+    Copyright @2016-{{ nowTear }} 上海靖璇网络科技有限公司 版权所有
   </ElFooter>
 </template>

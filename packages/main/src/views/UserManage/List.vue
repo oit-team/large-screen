70dcd32dfa945641e8da113b50a162a0fdc18ace
刘运双 <1048821976@qq.com>
2023-06-02 13:57:07 +0800

    feat: 新增订单、客户菜单

---
+++ packages/main/src/views/UserManage/List.vue
@@ -0,0 +1,26 @@
+<script>
+import { convertImageSize } from '@/utils/helper'
+
+export default {
+  name: 'UserManageList',
+  data: () => ({
+    data: {},
+  }),
+  computed: {
+  },
+  methods: {
+    convertImageSize,
+  },
+}
+</script>
+
+<template>
+  <div class="h-full p-2">
+    <!-- user-key : 1685517352639 -->
+    <!-- <TablePage ref="table" v-bind="tablePageOption" field-key="1669627381942" auto /> -->
+  </div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>

e6a05ee6a9f11db0ed55360fc0533f350bfdf0ba
刘运双 <1048821976@qq.com>
2023-06-02 13:57:07 +0800

    feat: 新增订单、客户菜单

---
+++ packages/main/src/api/userManage.js
@@ -0,0 +1,4 @@
+import { post } from './index'
+/**
+ * 客户管理
+ */
---
+++ packages/main/src/views/OrderManage/List.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'OrderManageList',
+  data: () => ({
+    data: {}, // 列表数据
+  }),
+  computed: {
+    tablePageOption() {
+      return {
+        promise: this.loadDate,
+        // action: [],
+      }
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="h-full p-2">
+    <!-- order : 1685523826849 -->
+    <!-- <TablePage v-bind="tablePageOption" ref="table" field-key="1669627381942" auto /> -->
+  </div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>

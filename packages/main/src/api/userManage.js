c064874f4cd1455e1d85807e866ec0c57ae6c8ec
刘运双 <1048821976@qq.com>
2023-06-13 15:30:51 +0800

    feat: 新增用户管理

--- packages/main/src/api/userManage.js
+++ packages/main/src/api/userManage.js
@@ -2,3 +2,6 @@ import { post } from './index'
 /**
  * 客户管理
  */
+export function getWeChatUserInfoList(params) {
+  return post('/system/weChatUser/getWeChatUserInfoList', params)
+}
--- packages/main/src/views/UserManage/List.vue
+++ packages/main/src/views/UserManage/List.vue
@@ -1,5 +1,6 @@
 <script>
 import { convertImageSize } from '@/utils/helper'
+import { getWeChatUserInfoList } from '@/api/userManage'
 
 export default {
   name: 'UserManageList',
@@ -7,17 +8,50 @@ export default {
     data: {},
   }),
   computed: {
+    tablePageOption() {
+      return {
+        promise: this.loadData,
+        table: {
+          data: this.data.resultList,
+        },
+        pager: {
+          total: this.data.pageTotal,
+        },
+      }
+    },
   },
   methods: {
     convertImageSize,
+    async loadData(params) {
+      const res = await getWeChatUserInfoList({
+        ...params,
+      })
+      this.data = res.body
+    },
   },
 }
 </script>
 
 <template>
   <div class="h-full p-2">
-    <!-- user-key : 1685517352639 -->
-    <!-- <TablePage ref="table" v-bind="tablePageOption" field-key="1669627381942" auto /> -->
+    <TablePage ref="table" v-bind="tablePageOption" field-key="1686635645706" auto>
+      <template #column:headPortrait>
+        <ElTableColumn
+          prop="headPortrait"
+          label="图片"
+          align="center"
+          width="120"
+        >
+          <div />
+          <template slot-scope="scope">
+            <ElImage v-if="scope.row.headPortrait" :src="convertImageSize(scope.row.headPortrait)" class="block w-70px h-70px mx-auto" fit="cover" />
+            <div v-else class="text-gray-300">
+              暂无
+            </div>
+          </template>
+        </ElTableColumn>
+      </template>
+    </TablePage>
   </div>
 </template>
 
